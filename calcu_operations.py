d63bf11300302dd6c4273cd54cd08bbc1b03a8ba
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-21 13:34:26 +0800

    pseudocode for operations file

---
+++ calcu_operations.py
@@ -0,0 +1,9 @@
+# Creating a calculator program using OOP concepts
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# Pseudocode (operations class)
+# create class operations
+    # if addition
+    # if subtraction
+    # if multi
+    # if divi
