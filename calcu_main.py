abc205bc938ce9bd4e84d7d036e82d0f7383b268
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-21 15:47:57 +0800

    ValueError is working

--- calcu_main.py
+++ calcu_main.py
@@ -11,23 +11,32 @@ class Calculator:
         self.operations = calcu_operations.Operations()
     # method for inputting values and calculation
     def calculation(self):
-        # Ask user to choose and input math operation
-        input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
-        # Ask user to input two numbers
-        first_number = float(input("Enter your first number: "))
-        second_number = float(input("Enter your second number: "))
-        # perform calculation using operations
-        result_number = self.operations.CalcOperation(first_number, second_number, input_operation)
-        # display result
-        print("Answer:", result_number)
+        try:
+            # Ask user to choose and input math operation
+            input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
+            # Ask user to input two numbers
+            first_number = float(input("Enter your first number: "))
+            second_number = float(input("Enter your second number: "))
+            # perform calculation using operations
+            result_number = self.operations.CalcOperation(first_number, second_number, input_operation)
 
-        # ask user if they want to try again or not
-        retry_input = input("Do you want to try again? (Yes/No): ")
-        # if yes repeat
-        if retry_input == "Yes":
-            Calculator.calculation(self)
-        else:
-            print("Thank you for using this calculator!")
+            if input_operation not in ['Addition', 'Subtraction', 'Multiplication', 'Division']:
+                raise ValueError("Invalid math operation")
+
+            # display result
+            print("Answer:", result_number)
+
+            # ask user if they want to try again or not
+            retry_input = input("Do you want to try again? (Yes/No): ")
+            # if yes repeat
+            if retry_input == "Yes":
+                Calculator.calculation(self)
+            else:
+                print("Thank you for using this calculator!")
+
+        # catch exceptions
+        except ValueError as e:
+            print(f"Error: {e}")
 
 # call calculation method
 calculator = Calculator()
