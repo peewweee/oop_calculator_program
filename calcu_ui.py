b9fe9aa11045fbcd360d68738f434a519a824db9
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-21 18:42:05 +0800

    adding header

--- calcu_ui.py
+++ calcu_ui.py
@@ -4,5 +4,11 @@
 import pyfiglet
 
 # Header
+def Header():
+    print("\033[0;33m ")
+    title_text = " Welcome to Phoebe's Calculator! "
+    title_line = title_text.center(150, "*")
+    print(title_line)
+    print("")
 # Loading animation
 # Thank you prompt
